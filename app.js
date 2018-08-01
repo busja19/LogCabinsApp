105e46f65a83b8ccb3b8ee3b18e25b57b64b26ab
diana_nevedomska <diana_nevedomska@yahoo.com>
2018-08-01 19:11:12 -0400

    jadeFiles

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ var app = express();
 const path = require('path');
 const VIEWS = path.join(__dirname, 'views');
 
+app.set('view engine', 'jade');
 
 app.use(express.static("scripts")); // allow app to use scripts
 app.use(express.static("images")); // allow app to use images
 
 app.get('/', function(req, res){
- // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
-  res.sendFile('index.html', {root: VIEWS});
+  res.render('index.jade', {root: VIEWS});
   console.log("Now you are home!");
 });
 
 
 // function to render the products page
+app.get('/products', function(req, res){
+   res.sendFile('products.html', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
+  console.log("Product Page!");
+});
+
+// function to render the FAQ page
 app.get('/faq', function(req, res){
  // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
-  res.render('faq.html', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
+  res.sendFile('faq.html', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
   console.log("FAQ PAge!");
 });
 
